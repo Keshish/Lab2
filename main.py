b683f0d509b438a64fb8f37ebb82bc6f4ea98b62
keshish <garo.keshish@gmail.com>
2023-10-26 17:05:43 +0200

    Almost Exercise 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 from Methods.BackwardEulerMethod import BackwardEulerMethod
 from Methods.CrankNicolsonMethod import CrankNicolsonMethod
 from Methods.EulerMethod import EulerMethod
@@ -7,10 +7,9 @@ from Rules.MidPointRule import MidPointRule
 from Rules.NewtonSolver import NewtonSolver
 from Rules.TrapezoidalRule import TrapezoidalRule
 from Rules.SimpsonRule import SimpsonsRule
-from Rules.ConvergenceAnalysis import ConvergenceAnalysis
+from Rules.NumericalIntegrationAnalyzer import NumericalIntegrationAnalyzer
 import matplotlib.pyplot as plt
 
-# Test the quadrature rules on ∫[0, π/2] sin(x) dx, known to be 1
 exact_value = 1
 a = 0
 b = math.pi / 2
@@ -21,51 +20,20 @@ midpoint_rule = MidPointRule(sin_func, a, b, n=10)
 trapezoidal_rule = TrapezoidalRule(sin_func, a, b, n=10)
 simpsons_rule = SimpsonsRule(sin_func, a, b, n=10)
 
-
-print(midpoint_rule.evaluate())
-print(trapezoidal_rule.evaluate())
-print(simpsons_rule.evaluate())
-
-# Define the integration interval
-a = 0
-b = math.pi / 2  # π/2
-
-# Initialize lists to store errors and h values
-max_num_subintervals = 8
-midpoint_analysis = ConvergenceAnalysis(MidPointRule, sin_func, a, b)
-trapezoidal_analysis = ConvergenceAnalysis(TrapezoidalRule, sin_func, a, b)
-simpsons_analysis = ConvergenceAnalysis(SimpsonsRule, sin_func, a, b)
-
-# Perform convergence analysis
-midpoint_analysis.compute_errors_and_convergence_rates(max_num_subintervals)
-trapezoidal_analysis.compute_errors_and_convergence_rates(max_num_subintervals)
-simpsons_analysis.compute_errors_and_convergence_rates(max_num_subintervals)
+print("1-a)\n")
+print(f'\t\tMidpoint\t{midpoint_rule.evaluate()}')
+print(f'\t\tTrapezoidal {trapezoidal_rule.evaluate()}')
+print(f'\t\tSimpsons\t{simpsons_rule.evaluate()}\n')
 
 
-# Estimate convergence rates
-midpoint_rates = midpoint_analysis.estimate_convergence_rate()
-trapezoidal_rates = trapezoidal_analysis.estimate_convergence_rate()
-simpsons_rates = simpsons_analysis.estimate_convergence_rate()
 
-# Limit the number of displayed convergence rates
-max_displayed_rates = 100  # You can adjust this as needed
+func = np.sin
+aa = np.pi / 2
+bb = 0
+nn = 10
 
-# Print the limited number of convergence rates
-print(f'Estimated Convergence Rate (Midpoint Rule): {midpoint_rates[:max_displayed_rates]}')
-print(f'Estimated Convergence Rate (Trapezoidal Rule): {trapezoidal_rates[:max_displayed_rates]}')
-print(f'Estimated Convergence Rate (Simpson\'s Rule): {simpsons_rates[:max_displayed_rates]}')
-
-# Plot the log-log graph
-# plt.figure(figsize=(10, 6))
-# plt.loglog(midpoint_analysis.hs, midpoint_analysis.errors, 'o-', label='Midpoint Rule', markersize=8)
-# plt.loglog(trapezoidal_analysis.hs, trapezoidal_analysis.errors, 's-', label='Trapezoidal Rule', markersize=8)
-# plt.loglog(simpsons_analysis.hs, simpsons_analysis.errors, '^-', label="Simpson's Rule", markersize=8)
-# plt.xlabel('h', fontsize=14)
-# plt.ylabel('Error', fontsize=14)
-# plt.title('Convergence Rates of Quadrature Methods', fontsize=16)
-# plt.legend()
-# plt.grid()
-# plt.show()
+analyzer = NumericalIntegrationAnalyzer(func, bb, aa, nn)
+analyzer.plot_convergence_rates()
 
 
 #####
@@ -78,7 +46,7 @@ print(f'Estimated Convergence Rate (Simpson\'s Rule): {simpsons_rates[:max_displ
 # This means that as you reduce the subinterval size (increase N), the error decreases quadratically.
 # However, if you use equal weights (wi = h for all i), the convergence rate will be reduced to O(h),
 # which means that the error decreases linearly as the subinterval size decreases.
-# In other words, the accuracy of the composite trapezoidal rule with equal weights
+# The accuracy of the composite trapezoidal rule with equal weights
 # will not improve as rapidly with smaller subintervals as it would with the original
 # rule that has unequal weights. This is because the original trapezoidal rule accounts
 # for the shape of the function by using the appropriate weights, whereas equal
@@ -96,15 +64,12 @@ T = 5.0
 N = 1000
 solver = NewtonSolver()  # Create a solver for nonlinear algebraic equations
 
-# Example usage for Euler method
 euler_solver = EulerMethod(y0, T, N)
 t_euler, y_euler = euler_solver.solve()
 
-# Example usage for Backward Euler method
 backward_euler_solver = BackwardEulerMethod(y0, T, N, solver)
 t_backward_euler, y_backward_euler = backward_euler_solver.solve()
 
-# Example usage for Crank-Nicolson method
 crank_nicolson_solver = CrankNicolsonMethod(y0, T, N, solver)
 t_crank_nicolson, y_crank_nicolson = crank_nicolson_solver.solve()
 
@@ -122,15 +87,17 @@ def f(y):
     return y - y**3
 
 
-y0 = 2.0
+y0 = 0.1
 T = 5.0
-N = 1000
+N = 8
+print("Start")
 solver = NewtonSolver()  # Create a solver for nonlinear algebraic equations
 
 # Example usage for Euler method
 euler_solver = EulerMethod(y0, T, N)
 t_euler, y_euler = euler_solver.solve()
 print("t_euler:\t" ,t_euler, y_euler)
+
 
 # Example usage for Backward Euler method
 backward_euler_solver = BackwardEulerMethod(y0, T, N, solver)
